9ccf5d897012d1883822dee5eaa8d69d9df81919
stefan <stefan.louie@gmail.com>
2021-06-20 17:30:42 +1000

    - Created MVC - Added in player states to the view

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,23 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+    {
+        public Player currentPlayer { get; set; }
+
+        public GameSession()
+        {
+            currentPlayer = new Player();
+            currentPlayer.Name = "HDMaster88";
+            currentPlayer.CharacterClass = "Sage";
+            currentPlayer.HitPoints = 100;
+            currentPlayer.ExperiencePoints = 0;
+            currentPlayer.Gold = 100;
+            currentPlayer.Level = 1;
+        }
+    }
+}
