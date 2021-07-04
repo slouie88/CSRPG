052afd553910362979079fff83ef10726fc55999
stefan <stefan.louie@gmail.com>
2021-07-04 14:48:41 +1000

    - Added in GameItem class and Weapon sub-class. - Added in ItemFactory class for GameItem creation.

---
+++ Engine/Factories/ItemFactory.cs
@@ -0,0 +1,31 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Engine.Factories
+{
+    static class ItemFactory
+    {
+        private static List<GameItem> _standardGameItems;
+
+        static ItemFactory()
+        {
+            _standardGameItems = new List<GameItem>();
+
+            _standardGameItems.Add(new Weapon(1001, "Heavy Rusty Sword", 1, 3, 5));
+            _standardGameItems.Add(new Weapon(1002, "Aged Wooden Sword", 1, 2, 3));
+        }
+
+        public static GameItem CreateGameItem(int itemTypeID)
+        {
+            GameItem standardItem = _standardGameItems.FirstOrDefault(item => item.ItemTypeID == itemTypeID);
+
+            if (standardItem != null)
+                return standardItem.Clone();
+
+            return null;
+        }
+    }
+}
---
+++ Engine/Models/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.Models
+{
+    class Weapon : GameItem
+    {
+        public int MinimumDamage { get; set; }
+
+        public int MaximumDamage { get; set; }
+
+        public Weapon(int itemTypeID, string name, int price, int minimumDamage, int maximumDamage) : base(itemTypeID, name, price)
+        {
+            MinimumDamage = minimumDamage;
+            MaximumDamage = maximumDamage;
+        }
+
+        public new Weapon Clone()
+        {
+            return new Weapon(ItemTypeID, Name, Price, MinimumDamage, MaximumDamage);
+        }
+    }
+}
