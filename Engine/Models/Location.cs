bf17dbf0362c7160ebc748a421e963f7b4c60e76
stefan <stefan.louie@gmail.com>
2021-07-04 14:51:00 +1000

    - Modified Location class to use auto properties.

--- Engine/Models/Location.cs
+++ Engine/Models/Location.cs
@@ -6,72 +6,15 @@ namespace Engine.Models
 {
     public class Location
     {
-        private int _XCoordinate;
-        private int _YCoordinate;
-        private string _name;
-        private string _description;
-        private string _imageName;
+        public int XCoordinate { get; set; }
 
+        public int YCoordinate { get; set; }
 
-        public int XCoordinate
-        {
-            get
-            {
-                return _XCoordinate;
-            }
-            set
-            {
-                _XCoordinate = value;
-            }
-        }
+        public string Name { get; set; }
 
-        public int YCoordinate
-        {
-            get
-            {
-                return _YCoordinate;
-            }
-            set
-            {
-                _YCoordinate = value;
-            }
-        }
+        public string Description { get; set; }
 
-        public string Name
-        {
-            get
-            {
-                return _name;
-            }
-            set
-            {
-                _name = value;
-            }
-        }
-
-        public string Description
-        {
-            get
-            {
-                return _description;
-            }
-            set
-            {
-                _description = value;
-            }
-        }
-
-        public string ImageName
-        {
-            get
-            {
-                return _imageName;
-            }
-            set
-            {
-                _imageName = value;
-            }
-        }
+        public string ImageName { get; set; }
 
     }
 }
