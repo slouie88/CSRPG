80e63bb4b8fc581eea785caed9771a3af964e760
stefan <stefan.louie@gmail.com>
2021-09-27 14:26:57 +1000

    Added MonsterEncounter class for functionality regarding monster encounters at locations.

---
+++ Engine/Models/MonsterEncounter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.Models
+{
+    class MonsterEncounter
+    {
+        public int MonsterID { get; set; }
+        public int ChanceOfEncountering { get; set; }
+
+        public MonsterEncounter(int monsterID, int chanceOfEncountering)
+        {
+            MonsterID = monsterID;
+            ChanceOfEncountering = chanceOfEncountering;
+        }
+    }
+}
