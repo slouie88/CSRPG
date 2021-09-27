3eb2650af50ca648736bdf689deb9c953ffb53ba
stefan <stefan.louie@gmail.com>
2021-09-27 12:08:39 +1000

    Added a random generator class to make the monster loot random.

---
+++ Engine/RandomNumberGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine
+{
+    class RandomNumberGenerator
+    {
+        private static readonly Random _generator = new Random();
+
+        public static int SimpleNumberBetween(int minValue, int maxValue)
+        {
+            return _generator.Next(minValue, maxValue + 1);
+        }
+    }
+}
